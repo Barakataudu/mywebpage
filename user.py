194b30f9a1f2a342884ca77e6cc2438acc678e73
muhandasa <adisataofeekabiodun@gmail.com>
2022-10-06 13:07:45 +0100

    userdb

---
+++ user.py
@@ -0,0 +1,4 @@
+firstname = input("FirstName:")
+Lastname = input("LastName:")
+program = input("Training Course:")
+print("Welcome {} to BT Impresa. Your {} training date will be communicate to you shortly".format(firstname, program))
